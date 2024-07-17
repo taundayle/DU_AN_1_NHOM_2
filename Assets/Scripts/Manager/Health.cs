92246f27e4decfbcd4811870c41d444c343dda84
Lê Thành Anh Tuấn <tuanltadayle@gmail.com>
2024-07-17 22:40:00 +0700

    fix
    fix lỗi quái không chết

--- Assets/Scripts/Manager/Health.cs
+++ Assets/Scripts/Manager/Health.cs
@@ -14,7 +14,7 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (health == 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
