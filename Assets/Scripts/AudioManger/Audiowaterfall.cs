b5e930cd4de0e9e89864c00fbebafe6c0fc7aa9f
Lê Thành Anh Tuấn <tuanltadayle@gmail.com>
2024-07-23 23:42:23 +0700

    update
    fix waterfall

---
+++ Assets/Scripts/AudioManger/Audiowaterfall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiowaterfall : MonoBehaviour
+{
+    [SerializeField] AudioClip watersound;
+    private AudioSource watersoundSource;  //nhảy
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        watersoundSource = GetComponent<AudioSource>();
+    }
+
+    private AudioClip WaterSound()
+    {
+        return watersound;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            watersoundSource.clip = watersound;
+            watersoundSource.Play();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            watersoundSource.clip = watersound;
+            watersoundSource.Stop();
+        }
+    }
+}
